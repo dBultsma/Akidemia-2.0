8ec1507eef5a69354fa35c562deaaf753b4431f3
Amy Jo <turneramyjo@gmail.com>
2018-05-03 12:17:43 +0100

    Merge remote-tracking branch 'origin/Fixng-Scene' into GUIEdit
    # Conflicts:
    #	src/sample/area1a.fxml

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -9,7 +9,6 @@ import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 import javafx.scene.*;
 import javafx.scene.image.*;
-import javafx.animation.*;
 import java.io.IOException;
 
 public class Controller {
@@ -64,64 +63,7 @@ public class Controller {
         window.show();
     }
 
-    public void toArea1(ActionEvent event) throws IOException {
 
-        //Getting the layout from file
-        Parent area1Layout = FXMLLoader.load(getClass().getResource("area1.fxml"));
-        //Used to get the current window
-        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        Scene area1Scene = new Scene(area1Layout, window.getWidth(), window.getHeight());
-        magCursor(area1Scene);
-        window.setScene(area1Scene);
-        window.show();
-    }
-
-
-    public void toArea2(ActionEvent event) throws IOException {
-
-        //Getting the layout from file
-        Parent area2Layout = FXMLLoader.load(getClass().getResource("area2.fxml"));
-        //Used to get the current window
-        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        Scene area2Scene = new Scene(area2Layout, window.getWidth(), window.getHeight());
-        magCursor(area2Scene);
-        window.setScene(area2Scene);
-        window.show();
-    }
-
-    public void toArea3(ActionEvent event) throws IOException {
-
-        //Getting the layout from file
-        Parent area3Layout = FXMLLoader.load(getClass().getResource("area3.fxml"));
-        //Used to get the current window
-        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        Scene area3Scene = new Scene(area3Layout, window.getWidth(), window.getHeight());
-        magCursor(area3Scene);
-        window.setScene(area3Scene);
-        window.show();
-    }
-
-    public void toArea4(ActionEvent event) throws IOException {
-
-        //Getting the layout from file
-        Parent area4Layout = FXMLLoader.load(getClass().getResource("area4.fxml"));
-        //Used to get the current window
-        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        Scene area4Scene = new Scene(area4Layout, window.getWidth(), window.getHeight());
-        magCursor(area4Scene);
-        window.setScene(area4Scene);
-        window.show();
-    }
-
-    public void toArea1a(ActionEvent event) throws IOException {
-
-        //Getting the layout from file
-        Parent area1aLayout = FXMLLoader.load(getClass().getResource("area1a.fxml"));
-        //Used to get the current window
-        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        window.setScene(new Scene(area1aLayout, window.getWidth(), window.getHeight()));
-        window.show();
-    }
 
 
     public void magCursor(Scene x) {
