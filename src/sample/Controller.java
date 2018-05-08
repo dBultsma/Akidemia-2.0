95f809310aeb6b4d197a6da1deacd090ef597d37
Ajc605 <36259418+Ajc605@users.noreply.github.com>
2018-05-08 12:14:26 +0100

    Revert "Merge pull request #3 from dBultsma/ImprovingMap"
    This reverts commit 9d920e6e778878870acc406d39bb4943e568ea3b, reversing
    changes made to 12049a6fe0eb4a36f639192ebd569f3103597abe.

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,6 +1,9 @@
 package sample;
 
+
+//import java.awt.event.ActionEvent;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
@@ -9,7 +12,10 @@ import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 import javafx.scene.*;
 import javafx.scene.image.*;
+
 import java.io.IOException;
+
+import static javafx.scene.image.ImageView.*;
 
 public class Controller {
 
@@ -27,7 +33,7 @@ public class Controller {
     public void toSettings(ActionEvent event) throws IOException {
 
         //Getting the layout from file
-        Parent SettingLayout = FXMLLoader.load(getClass().getResource("settings.fxml"));
+        Parent SettingLayout = FXMLLoader.load(getClass().getResource("settingmenu.fxml"));
         //Used to get the current window
         Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
         window.setScene(new Scene(SettingLayout, window.getWidth(), window.getHeight()));
@@ -63,7 +69,64 @@ public class Controller {
         window.show();
     }
 
+    public void toArea1(ActionEvent event) throws IOException {
 
+        //Getting the layout from file
+        Parent area1Layout = FXMLLoader.load(getClass().getResource("area1.fxml"));
+        //Used to get the current window
+        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        Scene area1Scene = new Scene(area1Layout, window.getWidth(), window.getHeight());
+        magCursor(area1Scene);
+        window.setScene(area1Scene);
+        window.show();
+    }
+
+
+    public void toArea2(ActionEvent event) throws IOException {
+
+        //Getting the layout from file
+        Parent area2Layout = FXMLLoader.load(getClass().getResource("area2.fxml"));
+        //Used to get the current window
+        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        Scene area2Scene = new Scene(area2Layout, window.getWidth(), window.getHeight());
+        magCursor(area2Scene);
+        window.setScene(area2Scene);
+        window.show();
+    }
+
+    public void toArea3(ActionEvent event) throws IOException {
+
+        //Getting the layout from file
+        Parent area3Layout = FXMLLoader.load(getClass().getResource("area3.fxml"));
+        //Used to get the current window
+        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        Scene area3Scene = new Scene(area3Layout, window.getWidth(), window.getHeight());
+        magCursor(area3Scene);
+        window.setScene(area3Scene);
+        window.show();
+    }
+
+    public void toArea4(ActionEvent event) throws IOException {
+
+        //Getting the layout from file
+        Parent area4Layout = FXMLLoader.load(getClass().getResource("area4.fxml"));
+        //Used to get the current window
+        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        Scene area4Scene = new Scene(area4Layout, window.getWidth(), window.getHeight());
+        magCursor(area4Scene);
+        window.setScene(area4Scene);
+        window.show();
+    }
+
+    public void toArea1a(ActionEvent event) throws IOException {
+
+        //Getting the layout from file
+        Parent area1aLayout = FXMLLoader.load(getClass().getResource("area1a.fxml"));
+        //Used to get the current window
+        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        window.setScene(new Scene(area1aLayout, window.getWidth(), window.getHeight()));
+        window.show();
+    }
 
 
     public void magCursor(Scene x) {
