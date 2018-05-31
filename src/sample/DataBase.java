804dcdc6ee7489c8d6d645b7a4f1deec3c0c0880
Ajc605 <36259418+Ajc605@users.noreply.github.com>
2018-05-31 15:52:51 +0100

    Clear database

--- src/sample/DataBase.java
+++ src/sample/DataBase.java
@@ -1,28 +1,11 @@
 package sample;
 
-import java.util.ArrayList;
+public class Database {
 
-public class DataBase {
+    //write
 
-    ArrayList <User> data = new ArrayList <User>();
+    //read
 
-    public void addUserToDataBase(String password) {
-        User newUser = new User(password);
-        data.add(newUser);
-    }
+    //check
 
-    public void viewDataBase(){
-        for(int i=0; i < data.size(); i++){
-            System.out.println("Data base " + i + ": " + data.get(i).getPassword());
-        }
-        System.out.println("End of data base");
-    }
-
-    public void checkPasswordFromDataBase(){
-        for(int i = 0; i < data.size(); i++)
-            if(data.get(i).getPassword().equals(password))
-                System.out.println("found");
-
-
-    }
 }
