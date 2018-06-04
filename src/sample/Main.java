a11beaacf2b4dada0ba4084c3cda84fe97c7f927
dBultsma <36738512+dBultsma@users.noreply.github.com>
2018-06-04 10:25:15 +0100

    changing start

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -35,7 +35,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource(
-                "map.fxml"));
+                "sample.fxml"));
         primaryStage.setTitle("Akidemia");
 
         primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
